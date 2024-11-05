5d14952f78929474e8f5f4fcd06e7a4922e82702
Riza <134340338+yumsha@users.noreply.github.com>
2024-11-05 20:32:28 +0700

    Add files via upload

---
+++ ArrayMethod.js
@@ -0,0 +1,31 @@
+// * (1) forEach
+let buah = ['Mangga', 'Apel', 'Jeruk'];
+buah.forEach((item, index) => {
+   console.log(`Buah ke-${index + 1}: ${item}`);
+});  
+// * output: [ [1] Mangga [2] Apel [3] Jeruk ]
+
+// * (2) pop
+let angkaangka = [1, 2, 3, 4, 5];
+angkaangka.pop();
+console.log(angka);
+// * output: [2 3 4 5]
+
+// * (3) Shift
+let angka = [1, 2, 3, 4, 5];
+angka.shift();
+console.log(angka);
+// * output: [1 2 3 4]
+
+// * (4) Slice
+const handphone = [Oppo, Vivo, Samsung, Xiaomi];
+handphone.slice (0,3);
+// * output: [0 Oppo, 3 Xiaomi]
+
+// * (5) Sort
+const Nomor = [11, 18, 100, 25, 30]
+Nomor.sort()
+// * output: [ 100, 11, 18, 25, 30 ]
+
+// Muhamad Riza Pahlevi
+// X PPLG 4
